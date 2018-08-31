18bdc63b44e7067e74e530bc9a0073dca4be1a63
RuslanMamedov <41539922+RuslanMamedov@users.noreply.github.com>
2018-08-31 15:35:38 -0700

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 28 13:56:58 2018
-
 @author: Mamedov
 """
 #Importing the libraries
@@ -45,28 +44,58 @@ from keras.layers import Dense #build hidden layers in between
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu', input_dim = 13))
+classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'softmax', input_dim = 13))
 
 # Adding the second hidden layer - apply grid search to find the optimal number of layers 
-classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu'))
 classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu'))
 
 # Adding the output layer
 classifier.add(Dense(output_dim = 5, init = 'uniform', activation = 'sigmoid')) 
 
 # Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
+classifier.compile(optimizer = 'rmsprop', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100) 
+classifier.fit(X_train, y_train, batch_size = 20, nb_epoch = 100) 
 
-# Part 3 - Making the predictions and evaluating the model
-
-# Predicting the Test set results
+# Part 3 - Making the predictions
 y_pred = classifier.predict(X_test)
-y_pred = np.rint(y_pred)
+y_pred = (y_pred > 0.5)
+y_pred = y_pred.astype(int)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-print (cm)
+
+# Part 4 - Evaluating, Improving and Tuning the ANN
+#Evaluating the model
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'softmax', input_dim = 13))
+    classifier.add(Dense(output_dim = 7, init = 'uniform', activation = 'relu'))
+    classifier.add(Dense(output_dim = 5, init = 'uniform', activation = 'sigmoid')) 
+    classifier.compile(optimizer = 'rmsprop', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+accuracies = cross_val_score (estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1 )
+mean = accuracies.mean()
+variance = accuracies.std()  #lower the variance, lower the bias (overfitting)
+print ('Mean: ',mean )
+print ('Variance: ',variance)
+ 
+
+# Tuning the ANN with grid search
+from sklearn.model_selection import GridSearchCV
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size' : [10, 20, 30],
+              'epochs' : [50, 100, 200],
+              'optimizer' : ['adam', 'rmsprop']}
+grid_search = GridSearchCV (estimator = classifier,
+                            param_grid = parameters,
+                            scoring = 'accuracy',
+                            cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+print ('Best accuracy: ',best_accuracy )
+print ('Best parameters: ',best_parameters)
